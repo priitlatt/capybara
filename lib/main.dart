5b43d6159f380fd9297c91e1effa986d3d17a63c
Stanislav Bondarenko <bondarenko.stanislav.96@gmail.com>
2019-12-02 18:26:04 +0200

    fail build

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,6 @@ class MyHomePage extends StatefulWidget {
   // case the title) provided by the parent (in this case the App widget) and
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
-
   final String title;
 
   @override
