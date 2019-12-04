e6ee79aae38ea77f842f392b58c5ef29fbe346b3
Stanislav Bondarenko <bondarenko.stanislav.96@gmail.com>
2019-12-04 14:23:55 +0200

    failed analyze

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    // return new MaterialApp(
+    String somevar = "somevar";
+    return new MaterialApp(
       title: 'Flutter CI',
       theme: new ThemeData(
         // This is the theme of your application.
