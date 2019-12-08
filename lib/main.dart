1ba0b0efbc88b5497f59e19dba00e02ae837df6e
Stanislav Bondarenko <bondarenko.stanislav.96@gmail.com>
2019-12-09 00:51:18 +0200

    fail tests and analyze

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    String no_use_var = "nousevar";
     return new MaterialApp(
       title: 'Flutter CI',
       theme: new ThemeData(
