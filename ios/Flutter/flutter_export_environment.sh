781196f4071a545ac74206ec8bdc77d329396dfc
Priit Lätt <lattpriit@gmail.com>
2020-06-02 14:48:41 +0300

    Add linux

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/priit/Library/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/priit/AndroidStudioProjects/capybara"
+export "FLUTTER_ROOT=/home/priit/programs/flutter"
+export "FLUTTER_APPLICATION_PATH=/tmp/capybara"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/priit/Library/flutter/bin/cache/artifacts/engine/ios-release"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/home/priit/programs/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.2"
 export "FLUTTER_BUILD_NUMBER=3"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
