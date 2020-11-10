65ef5248c1a15752af5d275c3987082b89184a38
Priit Lätt <lattpriit@gmail.com>
2020-11-10 13:01:08 +0200

    Add tests

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -5,4 +5,19 @@ void main() {
     var answer = 42;
     expect(answer, 42);
   });
+
+  test('my failing unit test', () {
+    var answer = 42;
+    expect(answer, 0);
+  });
+
+  test('my erroring unit test', () {
+    var result = 1 ~/ 0;
+    expect(result, 'infinity');
+  });
+
+  test('my skipped unit test', () {
+    var result = 1;
+    expect(result, '1');
+  }, skip: true);
 }
